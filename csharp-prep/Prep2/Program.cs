638dfd013ddef51e949018a54bbb4a358130a5f1
Nicolas Estoquin <nicolasabelestoquin@gmail.com>
2023-04-17 16:06:02 -0300

    Prep2 Done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade?: ");
+        string value = Console.ReadLine();
+        int p = int.Parse(value);
+        string finalLetter = "";
+
+        if ( p >= 90 ){
+            finalLetter = "A";
+        } else if ( p >= 80 ) {
+            finalLetter = "B";
+        } else if ( p >= 70 ) {
+            finalLetter = "C";
+        } else if ( p >= 60 ) {
+            finalLetter = "D";
+        } else {
+            finalLetter = "F";
+        }
+
+        // Extra points! :D
+        int lastDigit = -1;
+        string sign = "";
+        if ( p > 10 ) {
+            lastDigit = p % 10;
+        }
+        if ( (lastDigit >= 7) && (p < 97)) {
+            sign = "+";
+        } else  if ( ( lastDigit < 3 ) && ( p> 60 )) {
+            sign = "-";
+        } else {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {finalLetter}{sign}");
+
+        if ( p >= 70 ) {
+            Console.WriteLine("Congrats! you made it!.");
+        } else {
+            Console.WriteLine("Let's try again! you can do it!.");
+        }
     }
 }
