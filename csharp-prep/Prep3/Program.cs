93fb1da8ad0bcabb6eca4c4f88a8f99a10490975
Nicolas Estoquin <nicolasabelestoquin@gmail.com>
2023-04-19 16:15:02 -0300

    Prep3 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        char play = 'n';
+        do {
+            Console.Clear();
+            int random_number = new Random().Next(1, 100);
+            int guessInteger;
+            int attepmts = 1;
+            do {
+                Console.Write("What is your guess?: ");
+                guessInteger = int.Parse(Console.ReadLine());
+                if ( guessInteger !=  random_number ) {
+                    if ( guessInteger >  random_number ){
+                        Console.WriteLine("Lower");
+                    } else {
+                        Console.WriteLine("Higher");
+                    }
+                    attepmts++;
+                }
+            } while ( guessInteger !=  random_number);
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"Attempts: {attepmts}");
+            Console.WriteLine("Play again? [y/n]");
+            play = Console.ReadKey().KeyChar;
+        } while ( play.Equals('y') );
+        
     }
 }
